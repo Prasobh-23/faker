f40acd9ac0eb1ae34a06e4394452f058dafd83dc
Prasobh-23 <prasobh.kylm96@gmail.com>
2022-04-30 12:36:35 +0530

    first code commit

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# faker
-Python program to generate fake info and insert it to the database
+# fakerpython
+
+
+>Python program to generate fake info and insert it to the database
+It is completely written in Python ⌨️.
+
+# Download code 📥
+
+* git clone https://github.com/Prasobh-23/ipLoc.git
+
+
+# Libraries need ⚙️
+
+* Faker             #To generate fake information
+* mysql.connector   #To connect to the database
+* import time             
+* tqdm              #To show progressbar
+  
+# Install libraries 📥
+
+* pip install Faker
+* pip install tqdm
+* pip install mysql-connector-python
+
+>This commands will install the python libraries required to run fakerpython
+
+>Explanations of the code is written along with the code
---
+++ generatefakedb.py
@@ -0,0 +1,41 @@
+from faker import Faker #To generate fake information
+import mysql.connector  #To connect to the database
+import time             
+from tqdm import tqdm   #To show progressbar
+
+fake = Faker()
+list=[]
+
+for i in tqdm(range(0,11000)):
+    list.append([(fake.name()),fake.address(),fake.job()])
+
+# The above for-loop is used to generate fake information. You can change the range 11000 to your need. 
+# But always keep in mind the list in python has a limit in capacity.
+# The loop will generate random fake info in each iteration and it will be appended to the list.
+# You can also generate another type of information like ipv4 addresses, Currency, etc.
+# Visit https://faker.readthedocs.io/en/master/ for more info about faker.
+
+# The codes written below are used to establish a connection with the database and perform the query.
+conn = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password = "",
+    database = "fakedata"
+    )
+cur = conn.cursor()
+sql = "INSERT INTO fake (name, address, designation) VALUES(%s,%s,%s)"
+
+try:
+    cur.executemany(sql, list)
+    conn.commit()
+    print("Data has been uploaded successfuly")
+    message = "Process finished successfuly. "
+except:
+    print("Error! Check db connection or try a lower range in the loop")
+    message = "Process ended with an error!  "
+
+finally:
+    print(message + "Exiting....")
+    cur.close()
+    conn.close()
+    time.sleep(3)
